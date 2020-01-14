ada666adea24fc1ee2167b0b0c57705af17cad68
Andrew Lampinen <lampinen@stanford.edu>
2020-01-14 15:06:35 -0800

    capturing play

--- grid_tasks.py
+++ grid_tasks.py
@@ -548,14 +548,27 @@ class Environment(object):
 
 def main(argv=()):
     np.random.seed(0)
-    env = Environment(GameDef("pusher", "pink", "ocean", False, False))
-    obs, r, done = env.reset()
-    plot.imshow(obs)
-    plot.savefig("first.png")
-    for i in range(5):
-        obs, r, done = env.step(i)
-        plot.imshow(obs)
-        plot.savefig("%i.png" % i)
+    for game_type, cols in zip(["pusher", "pick_up"], 
+                               [("red", "blue"), 
+                                ("forest", "orange")]):
+        env = Environment(GameDef(game_type, cols[0], cols[1], False, False))
+        obs, r, done = env.reset()
+        fig = plot.figure(frameon=False)
+        fig.set_size_inches(3, 3)
+        ax = plot.Axes(fig, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+        plot.imshow(obs, aspect='auto')
+        plot.savefig("%s_0.png" % game_type)
+        for i in range(5):
+            obs, r, done = env.step(i)
+            fig = plot.figure(frameon=False)
+            fig.set_size_inches(3, 3)
+            ax = plot.Axes(fig, [0., 0., 1., 1.])
+            ax.set_axis_off()
+            fig.add_axes(ax)
+            plot.imshow(obs, aspect='auto')
+            plot.savefig("%s_%i.png" % (game_type, i + 1))
 
     for game_type in ["pusher", "shooter", "sequence_imitation", "pick_up"]:
         game = make_game(game_type, "red", "blue")
